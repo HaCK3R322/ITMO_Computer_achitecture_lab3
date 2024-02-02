ec80bb34af3fac14abeeb7c9f6f72d57f1581f84
dictator_zx <ivanandrosovv@gmail.com>
2024-02-03 01:15:06 +0300

    Test on testing!!

--- src/start.py
+++
@@ -1,5 +0,0 @@
-from translatorv2 import main as translator_main
-from model import main as model_main
-
-translator_main("source.forth", "program.lab")
-model_main("program.lab", "input.txt", "out.txt")
