c77551eaf3a247307967c048fd78ab6d111965f4
dictator_zx <ivanandrosovv@gmail.com>
2024-02-03 00:35:01 +0300

    Fixing workflow... 8

--- test/unittests/translator/TestTranslator.py
+++ test/unittests/translator/TestTranslator.py
@@ -1,4 +1,5 @@
 import json
+import os
 import unittest
 from src.translatorv2 import Translator
 
@@ -7,10 +8,18 @@ class TestTranslator(unittest.TestCase):
     def setUp(self):
         print(self._testMethodDoc)
         self.instructions_start_address = 0x00C0
+        self.script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    def get_source_and_expected_paths(self, name):
+        source_path = os.path.join(self.script_directory, name, 'source.forth')
+        expected_path = os.path.join(self.script_directory, name, 'expected.lab')
+        return source_path, expected_path
 
     def test_void(self):
-        with open('void/source.forth') as source_file:
-            with open('void/expected.lab') as expected_file:
+        source_path, expected_path = self.get_source_and_expected_paths('void')
+
+        with open(source_path) as source_file:
+            with open(expected_path) as expected_file:
                 source = source_file.read()
                 translator = Translator(source)
                 translator.translate()
@@ -20,8 +29,10 @@ class TestTranslator(unittest.TestCase):
                 self.assertEqual(translated_instructions, expected_instructions)
 
     def test_nested_ifs(self):
-        with open('nested_ifs/source.forth') as source_file:
-            with open('nested_ifs/expected.lab') as expected_file:
+        source_path, expected_path = self.get_source_and_expected_paths('nested_ifs')
+
+        with open(source_path) as source_file:
+            with open(expected_path) as expected_file:
                 source = source_file.read()
                 translator = Translator(source)
                 translator.translate()
@@ -70,8 +81,10 @@ class TestTranslator(unittest.TestCase):
             translator.translate()
 
     def test_begin_until_inside_function(self):
-        with open('begin_until_inside_function/source.forth') as source_file:
-            with open('begin_until_inside_function/expected.lab') as expected_file:
+        source_path, expected_path = self.get_source_and_expected_paths('begin_until_inside_function')
+
+        with open(source_path) as source_file:
+            with open(expected_path) as expected_file:
                 source = source_file.read()
                 translator = Translator(source)
                 translator.translate()
@@ -81,8 +94,10 @@ class TestTranslator(unittest.TestCase):
                 self.assertEqual(translated_instructions, expected_instructions)
 
     def test_begin_until_inside_if_then(self):
-        with open('begin_until_inside_if_then/source.forth') as source_file:
-            with open('begin_until_inside_if_then/expected.lab') as expected_file:
+        source_path, expected_path = self.get_source_and_expected_paths('begin_until_inside_if_then')
+
+        with open(source_path) as source_file:
+            with open(expected_path) as expected_file:
                 source = source_file.read()
                 translator = Translator(source)
                 translator.translate()
@@ -92,8 +107,10 @@ class TestTranslator(unittest.TestCase):
                 self.assertEqual(translated_instructions, expected_instructions)
 
     def test_nested_begin_inside_nested_if_inside_function_inside_begin(self):
-        with open('nested_begin_inside_nested_if_inside_function_inside_begin/source.forth') as source_file:
-            with open('nested_begin_inside_nested_if_inside_function_inside_begin/expected.lab') as expected_file:
+        source_path, expected_path = self.get_source_and_expected_paths('nested_begin_inside_nested_if_inside_function_inside_begin')
+
+        with open(source_path) as source_file:
+            with open(expected_path) as expected_file:
                 source = source_file.read()
                 translator = Translator(source)
                 translator.translate()
@@ -103,170 +120,3 @@ class TestTranslator(unittest.TestCase):
 
                 self.assertEqual(translated_instructions, expected_instructions)
 
-
-
-
-#
-#     def test_translate_if_then(self):
-#         code = "IF 10 THEN"
-#         translator = Translator(code, optimize=True)
-#
-#         instructions, data = translator.translate()
-#
-#         expected_instructions = [{'opcode': 'PUSH', 'address': 2049, 'related_token_index': 2},
-#                                  {'opcode': 'CMP', 'related_token_index': 2},
-#                                  {'opcode': 'JZ', 'address': 4, 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'PUSH', 'address': 2050, 'related_token_index': 1},
-#                                  {'opcode': 'JMP', 'address': 2, 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'HLT'}]
-#
-#         self.assertEqual(instructions, expected_instructions)
-#
-#     def test_translate_loop(self):
-#         code = "BEGIN 228 UNTIL"
-#         translator = Translator(code, optimize=True)
-#         instructions, data = translator.translate()
-#
-#         expected_instructions = [{'opcode': 'PUSH', 'address': 2050, 'related_token_index': 1},
-#                                  {'opcode': 'PUSH', 'address': 2049, 'related_token_index': 2},
-#                                  {'opcode': 'CMP', 'related_token_index': 2},
-#                                  {'opcode': 'JNZ', 'address': 3, 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'JMP', 'address': -7, 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'DROP', 'related_token_index': 2},
-#                                  {'opcode': 'HLT'}]
-#
-#         self.assertEqual(instructions, expected_instructions)
-#
-#     def test_translate_nested_loop(self):
-#         code = "BEGIN 10 BEGIN 20 UNTIL UNTIL"
-#         translator = Translator(code, optimize=True)
-#         instructions, data = translator.translate()
-#
-#         expected_instructions = [{'opcode': 'PUSH', 'address': 2050, 'related_token_index': 1},
-#                                  {'opcode': 'PUSH', 'address': 2051, 'related_token_index': 3},
-#                                  {'opcode': 'PUSH', 'address': 2049, 'related_token_index': 4},
-#                                  {'opcode': 'CMP', 'related_token_index': 4},
-#                                  {'opcode': 'JNZ', 'address': 3, 'related_token_index': 4},
-#                                  {'opcode': 'DROP', 'related_token_index': 4},
-#                                  {'opcode': 'DROP', 'related_token_index': 4},
-#                                  {'opcode': 'JMP', 'address': -7, 'related_token_index': 4},
-#                                  {'opcode': 'DROP', 'related_token_index': 4},
-#                                  {'opcode': 'DROP', 'related_token_index': 4},
-#                                  {'opcode': 'PUSH', 'address': 2049, 'related_token_index': 5},
-#                                  {'opcode': 'CMP', 'related_token_index': 5},
-#                                  {'opcode': 'JNZ', 'address': 3, 'related_token_index': 5},
-#                                  {'opcode': 'DROP', 'related_token_index': 5},
-#                                  {'opcode': 'DROP', 'related_token_index': 5},
-#                                  {'opcode': 'JMP', 'address': -16, 'related_token_index': 5},
-#                                  {'opcode': 'DROP', 'related_token_index': 5},
-#                                  {'opcode': 'DROP', 'related_token_index': 5},
-#                                  {'opcode': 'HLT'}]
-#
-#         self.assertEqual(instructions, expected_instructions)
-#
-#     def test_variable_declaraion_and_usage(self):
-#         code = "variable a a"
-#         translator = Translator(code, optimize=True)
-#         instructions, data = translator.translate()
-#
-#         expected_instructions = [{'opcode': 'PUSH', 'address': 0x802, 'related_token_index': 2},
-#                                  {'opcode': 'HLT'}]
-#
-#         expected_data = [-1, 0, 0x803, 0]
-#
-#         self.assertEqual(instructions, expected_instructions)
-#         self.assertEqual(data, expected_data)
-#
-#     def test_variable_digit_name(self):
-#         code = "variable 41"
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#     def test_varaible_redeclaration(self):
-#         code = "variable a variable a"
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#     def test_variable_using_defined_names(self):
-#         code = "variable IF"
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#         code = "variable +"
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#         code = "variable UNTIL"
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#     def test_unexpected_token(self):
-#         code = "2 48 + . this_is_unexpected_token BEGIN IF THEN UNTIL"
-#
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#     def test_not_closed_loop(self):
-#         code = "BEGIN UNTIL BEGIN BEGIN  UNTIL UNTIL 111 BEGIN BEGIN UNTIL"
-#
-#         translator = Translator(code, optimize=True)
-#
-#         with self.assertRaises(SyntaxError):
-#             translator.translate()
-#
-#     def test_data_optimize_translation(self):
-#         code = '1 1 1 2 2 2 3 3 3'
-#
-#         translator_optimize_false = Translator(code, optimize=False)
-#         translator_optimize_true = Translator(code, optimize=True)
-#
-#         instructions, data_optimize_false = translator_optimize_false.translate()
-#         instructions, data_optimized_true = translator_optimize_true.translate()
-#
-#         self.assertNotEqual(data_optimized_true, data_optimize_false)
-#
-#         excpected_data_optimize_false = [-1, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
-#         excpected_data_optimize_true = [-1, 0, 1, 2, 3]
-#
-#         self.assertEqual(data_optimize_false, excpected_data_optimize_false)
-#         self.assertEqual(data_optimized_true, excpected_data_optimize_true)
-#
-#     def test_define_and_use_function(self):
-#         code = ":func1 1 2 ; func1 func1"
-#
-#         translator = Translator(code, optimize=True)
-#         instructions, data = translator.translate()
-#
-#         expected_instructions = [{'opcode': 'PUSH', 'address': 2050, 'related_token_index': 1},
-#                                  {'opcode': 'PUSH', 'address': 2051, 'related_token_index': 2},
-#                                  {'opcode': 'PUSH', 'address': 2050, 'related_token_index': 1},
-#                                  {'opcode': 'PUSH', 'address': 2051, 'related_token_index': 2},
-#                                  {'opcode': 'HLT'}]
-#
-#         expected_data = [-1, 0, 1, 2]
-#
-#         self.assertEqual(expected_instructions, instructions)
-#         self.assertEqual(expected_data, data)
-#
-#
-# if __name__ == "__main__":
-#     unittest.main(verbosity=2)
